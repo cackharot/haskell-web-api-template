f67a31210e3c945e39f2af30d647b15e75cc6b72
cackharot <cackharot@gmail.com>
2021-01-02 15:47:48 -0500

    Refactor info & health endpoints as middlewares

--- src/Run.hs
+++ src/Run.hs
@@ -4,16 +4,16 @@
 {-# LANGUAGE TypeOperators     #-}
 module Run (runApi) where
 
-import           Battery
 import           Import
 
-import           Network.HTTP.Types                   (status200)
-import           Network.HTTP.Types.Header            (hContentType)
-import           Network.Wai                          (Application, responseLBS)
+import           Data.Aeson                           (encode)
+import           Network.Wai                          (Application)
 import           Network.Wai.Handler.Warp             (defaultSettings,
                                                        runSettings,
                                                        setBeforeMainLoop,
                                                        setPort)
+import           Network.Wai.Middleware.Health        (health)
+import           Network.Wai.Middleware.Info          (info)
 import qualified Network.Wai.Middleware.Prometheus    as P
 import           Network.Wai.Middleware.RequestLogger (logStdout, logStdoutDev)
 import qualified Prometheus                           as P
@@ -22,40 +22,40 @@ import           Servant
 import           System.Environment                   (lookupEnv)
 import           System.IO                            (hPutStrLn, stderr)
 
-type API = BatteryAPI
-  :<|> "empty" :> EmptyAPI
+type API = EmptyAPI
 
 appAPI:: Proxy API
 appAPI = Proxy
 
-appServer infoDetail = (batteryServer infoDetail) :<|> emptyServer
+appServer :: ServerT EmptyAPI m
+appServer = emptyServer
 
-app1 :: InfoDetail -> Application
-app1 infoDetail = serve appAPI $ appServer infoDetail
+app :: Application
+app = serve appAPI $ appServer
 
--- app :: Application
--- app req f =
---     f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
-
-appMiddlewares :: Bool -> Application -> Application
-appMiddlewares isProdEnv = logType . (P.prometheus P.def)
+appMiddlewares :: Bool -> InfoDetail -> Application -> Application
+appMiddlewares isProdEnv infoDetail = logType . P.prometheus P.def . health . info jsonInfoDetail
   where
     logType = if isProdEnv then logStdout else logStdoutDev
+    jsonInfoDetail = encode infoDetail
 
 runApi :: RIO App ()
 runApi = do
   hSetBuffering stdin LineBuffering
-  app <- ask
-  eport <- liftIO $ fromMaybe (port $ appOptions app ) . (>>= readMaybe) <$> lookupEnv "PORT"
+  appConfig <- ask
+  eport <-
+    liftIO $
+    fromMaybe (port $ appOptions appConfig) . (>>= readMaybe) <$>
+    lookupEnv "PORT"
   let settings =
         setPort eport $
         setBeforeMainLoop
           -- (logInfo $ "Started application on " <> (fromString $ show port)) $
-          (hPutStrLn stderr $ "Started application on " ++ show eport) $
-        defaultSettings
+          (hPutStrLn stderr $ "Started application on " ++ show eport)
+          defaultSettings
   _ <- P.register P.ghcMetrics
   isProdEnv <-
-    liftIO $ do
-      isProdEnv <- maybe False (== "PRODUCTION") <$> lookupEnv "APP_ENVIRONMENT"
-      return isProdEnv
-  liftIO $ runSettings settings $ appMiddlewares isProdEnv $ app1 (appInfoDetail app)
+    liftIO $ do maybe False (== "PRODUCTION") <$> lookupEnv "APP_ENVIRONMENT"
+  liftIO $
+    runSettings settings $
+    appMiddlewares isProdEnv (appInfoDetail appConfig) $ app
