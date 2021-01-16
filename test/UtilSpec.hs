9e26acfe07f5cf260543e8971e9db70fb5978866
cackharot <cackharot@gmail.com>
2021-01-16 17:14:13 -0500

    Add dummy test & fix CI

--- test/UtilSpec.hs
+++ test/UtilSpec.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+
 module UtilSpec (spec) where
 
-import Import
-import Util
+import RIO
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = do
-  describe "plus2" $ do
-    it "basic check" $ plus2 0 `shouldBe` 2
-    it "overflow" $ plus2 maxBound `shouldBe` minBound + 1
-    prop "minus 2" $ \i -> plus2 i - 2 `shouldBe` i
+  describe "Bool" $ do
+    it "works with True" $
+      True `shouldBe` True
+
+    it "works with False" $
+      False `shouldBe` False
+
+    it "works with True and False" $
+      True `shouldBe` not False
